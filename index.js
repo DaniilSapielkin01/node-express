a4dae0e5a44ad7b38a45bfe66d96f0bbaa728097
Daniil_Sapielkin <vipdanik98@gmail.com>
2020-12-23 00:36:23 +0200

    part 3 lesson 10

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const path = require("path");
+const exphbs = require("express-handlebars");
+const homeRoutes = require("./routers/home");
+const addRoutes = require("./routers/add");
+const coursesRoutes = require("./routers/courses");
+
+const app = express();
+
+const hbs = exphbs.create({
+  defaultLayout: "main",
+  extname: "hbs", //вместо имени handlebars
+}); //передем кофигурируемый обьект
+
+app.engine("hbs", hbs.engine); //Происходит регистрация такого движка
+app.set("view engine", "hbs"); //тут с помощью set мы его уже нач-ем испол-ть
+app.set("views", "views"); //2й парамент  где хранятся все наши шаблоны
+
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/", homeRoutes);
+app.use("/add", addRoutes);
+app.use("/courses", coursesRoutes);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT} .........`);
+});
