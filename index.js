c6b10998d35e996d2efa03c38f31e7fdebc87766
Daniil_Sapielkin <vipdanik98@gmail.com>
2020-12-29 01:44:13 +0200

    login page

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const path = require("path");
 const mongoose = require("mongoose");
 const exphbs = require("express-handlebars");
+const session = require("express-session");
 
 const homeRoutes = require("./routes/home");
 const cardRoutes = require("./routes/card");
@@ -10,6 +11,7 @@ const ordersRoutes = require("./routes/orders");
 const coursesRoutes = require("./routes/courses");
 const authRoutes = require("./routes/auth");
 const User = require("./models/user");
+const varMiddleware = require("./middleware/variables");
 
 const app = express();
 
@@ -28,18 +30,26 @@ app.engine("hbs", hbs.engine);
 app.set("view engine", "hbs");
 app.set("views", "views");
 
-app.use(async (req, res, next) => {
-  try {
-    const user = await User.findById("5fe87e4d631b9440f92ac83c");
-    req.user = user;
-    next();
-  } catch (e) {
-    console.log(e);
-  }
-});
+// app.use(async (req, res, next) => {
+//   try {
+//     const user = await User.findById("5fe87e4d631b9440f92ac83c");
+//     req.user = user;
+//     next();
+//   } catch (e) {
+//     console.log(e);
+//   }
+// });
 
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
+app.use(
+  session({
+    secret: "some secret value",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+app.use(varMiddleware);
 
 app.use("/", homeRoutes);
 app.use("/add", addRoutes);
