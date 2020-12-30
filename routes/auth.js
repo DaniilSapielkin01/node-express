d1742e8c146ef7b71befda0e390aa58583a3b391
Daniil_Sapielkin <vipdanik98@gmail.com>
2020-12-31 00:01:55 +0200

    add validate

--- routes/auth.js
+++ routes/auth.js
@@ -4,8 +4,9 @@ const bcrypt = require("bcryptjs");
 // const sendGrid = require("nodemailer-sendgrid-transport");// with const transporter
 const crypto = require("crypto");
 const sgMail = require("@sendgrid/mail");
-const { body, validationResult } = require("express-validator/check");
+const { validationResult } = require("express-validator/check");
 
+const { registerValidators } = require("../utils/validators");
 const registerEmail = require("../emails/registration");
 const resetEmail = require("../emails/reset");
 const keys = require("../keys");
@@ -65,32 +66,26 @@ router.post("/login", async (req, res) => {
   }
 });
 
-router.post("/register", body("email").isEmail(), async (req, res) => {
+router.post("/register", registerValidators, async (req, res) => {
   try {
-    const { email, password, confirm, name } = req.body;
-    const candidate = await User.findOne({ email });
+    const { email, password, name } = req.body;
+
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       req.flash("registerError", errors.array()[0].msg);
       return res.status(422).redirect("/auth/login#register");
-    }
-
-    if (candidate) {
-      req.flash("registerError", "Пользователь с таким email уже существует");
-      res.redirect("/auth/login#register");
-    } else {
-      const hashPassword = await bcrypt.hash(password, 12);
-      const user = new User({
-        email,
-        name,
-        password: hashPassword,
-        cart: { items: [] },
-      });
-      await user.save();
-      res.redirect("/auth/login#login");
-      // await transporter.sendMail(registerEmail(email));
-      await sgMail.send(registerEmail(email));
-    }
+    } 
+    const hashPassword = await bcrypt.hash(password, 12);
+    const user = new User({
+      email,
+      name,
+      password: hashPassword,
+      cart: { items: [] },
+    });
+    await user.save();
+    res.redirect("/auth/login#login");
+    // await transporter.sendMail(registerEmail(email));
+    await sgMail.send(registerEmail(email));
   } catch (e) {
     console.log(e);
   }
