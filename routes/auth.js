ddefcd81994a5b5639a410a49a581b5a3efd5cb4
Daniil_Sapielkin <vipdanik98@gmail.com>
2020-12-30 23:21:17 +0200

    locked courses 69lessons

--- routes/auth.js
+++ routes/auth.js
@@ -115,8 +115,6 @@ router.get("/password/:token", async (req, res) => {
         token: req.params.token,
       });
     }
-    
-
   } catch (e) {
     console.log(e);
   }
@@ -142,6 +140,28 @@ router.post("/reset", (req, res) => {
         res.redirect("/auth/reset");
       }
     });
+  } catch (e) {
+    console.log(e);
+  }
+});
+
+router.post("/password", async (req, res) => {
+  try {
+    const user = await User.findOne({
+      _id: req.body.userId,
+      resetToken: req.body.token,
+      resetTokenExp: { $gt: Date.now() }, // означает что resetTOkenExp > чем $gt:  ...
+    });
+    if (user) {
+      user.password = await bcrypt.hash(req.body.password, 12);
+      user.resetToken = undefined;
+      user.resetTokenExp = undefined;
+      await user.save();
+      res.redirect("/auth/login");
+    } else {
+      req.flash("loginError", "Время действия ссылки истекло");
+      res.redirect("/auth/login");
+    }
   } catch (e) {
     console.log(e);
   }
